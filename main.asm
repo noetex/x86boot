9955701d0c48ce1594584efd8fa1d4468bc840a8
Lazuli Ziloet <rneutron@protonmail.com>
2023-05-17 06:02:23 +0300

    added print_string for debugging

--- main.asm
+++ main.asm
@@ -1,1 +1,27 @@
-jmp $
+[bits 16]
+[org 0x7c00]
+
+main:
+		mov bx, Message
+		call print_string
+		jmp $
+
+print_string:
+		pusha
+		mov ah, 0x0e	;bios teletype output routine
+	print_string_loop:
+		mov al, [bx]	;bx: pointer to char string
+		cmp al, 0
+		je print_string_end
+		int 0x10
+		inc bx
+		jmp print_string_loop
+	print_string_end:
+		popa
+		ret
+
+Message db "Hello World", 0
+
+; MBR signature
+times (510 - ($ - $$)) db 0
+dw 0xaa55
