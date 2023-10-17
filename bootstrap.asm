50e61916e0be9444195cc67b47aa8425a590730f
Rami Abbas <145647517+noetex@users.noreply.github.com>
2023-10-17 19:10:18 +0300

    Postpone switching to 64-bit mode
    The bootsector is too small for the code to enable long mode, since it requires
    reserving space for page tables; so we'll leave that job to the second stage
    bootloader, which is Work-In-Progress. Therefore we must compile main.c as a
    32-bit binary.

--- bootstrap.asm
+++ bootstrap.asm
@@ -2,20 +2,6 @@
 [BITS 16]
 		jmp 0x0: label_start16  ; clear cs register prior to any execution
 
-routine_print_string:
-		pusha
-		mov ah, 0xe    ; teletype output bios routine
-.print_string_loop:
-		mov al, [bx]
-		test al, al    ; end of string?
-		jz .pring_string_end
-		int 0x10
-		inc bx
-		jmp .print_string_loop
-.pring_string_end:
-		popa
-		ret
-
 label_start16:
 		xor ax, ax
 		mov ds, ax
@@ -28,18 +14,49 @@ label_start16:
 		mov dh, 0x0         ; source head
 		mov cl, 0x2         ; sector index (one-based)
 		int 0x13            ; TODO: error checking
+		jnc label_enter_protected_mode
+		mov si, MESSAGE_DISK_ERROR
+		mov ah, 0xe         ; teletype output bios routine
+.print_message_loop:
+		mov al, [si]
+		test al, al    ; end of string?
+		jz .print_message_end
+		int 0x10
+		inc si
+		jmp .print_message_loop
+.print_message_end:
+		jmp $
 
-		; enter 32-bit protected mode
+label_enter_protected_mode:
 		cli
-		lgdt [gdt_desc_begin]
+		lgdt [GDT32_DESC]
 		mov eax, cr0
 		or eax, 1
 		mov cr0, eax
 		jmp CODE_SEG_OFFSET: label_start32  ; flush instruction pipeline
 
 [bits 32]
-routine_print_string_32bit:
-		pusha
+label_start32:
+		mov ax, DATA_SEG_OFFSET
+		mov ds, ax
+		mov es, ax
+		mov ss, ax
+
+		mov esp, 0x90000
+		mov ebp, esp
+
+		; does our hardware support the CPUID instruction?
+		pushfd
+		pop eax             ; read the flags register
+		mov ecx, eax
+		xor eax, (1 << 21)  ; attempt to toggle the ID bit
+		push eax
+		popfd               ; update the flags register
+		pushfd
+		pop eax             ; read the value again
+		cmp eax, ecx        ; was that bit toggled succesfully?
+		jne label_cpuid_supported
+		mov esi, MESSAGE_UNSUPPORTED_CPUID
 		mov edi, VIDEO_MEMORY
 .print_string_32bit_loop:
 		mov al, [esi]
@@ -51,53 +68,11 @@ routine_print_string_32bit:
 		add edi, 2
 		jmp .print_string_32bit_loop
 .print_string_32bit_end:
-		popa
-		ret
+		jmp $
 
-label_start32:
-		mov ax, DATA_SEG_OFFSET
-		mov ds, ax
-		mov es, ax
-		mov ss, ax
-
-		mov esp, 0x90000
-		mov ebp, esp
-
-;		; does our hardware support the CPUID instruction?
-;		pushfd
-;		pop eax             ; read the flags register
-;		mov ecx, eax
-;		xor eax, (1 << 21)  ; attempt to toggle the ID bit
-;		push eax
-;		popfd               ; update the flags register
-;		pushfd
-;		pop eax             ; read the value again
-;		cmp eax, ecx        ; was that bit toggled succesfully?
-;		jne label_cpuid_supported
-;		mov esi, MESSAGE_UNSUPPORTED_CPUID
-;		call routine_print_string_32bit
-;		jmp $
-;
-;label_cpuid_supported:
-;		; push ecx          ; NOTE: do we have to do this?
-;		; popfd             ; restore old flags state
-;
-;		; does our hardware support 64-bit mode?
-;		mov eax, 0x80000000
-;		cpuid
-;		cmp eax, 0x80000001
-;		jb label_long_mode_unsupported
-;		mov eax, 0x80000001
-;		cpuid
-;		test edx, (1 << 29)
-;		jnz label_long_mode_supported
-;
-;label_long_mode_unsupported:
-;		mov esi, MESSAGE_UNSUPPORTED_64BIT
-;		call routine_print_string_32bit
-;		jmp $
-
-label_long_mode_supported:
+label_cpuid_supported:
+		; push ecx          ; NOTE: do we have to do this?
+		; popfd             ; restore old flags state
 		call KERNEL_CODE
 
 label_infinite_halt:
@@ -106,20 +81,15 @@ label_infinite_halt:
 
 
 ; data
-gdt_begin:
-	gdt_null: dq 0x0
-	gdt_code: dq 0x00cf9a000000ffff
-	gdt_data: dq 0x00cf92000000ffff
-gdt_end:
-
-gdt_desc_begin:
-	dw GDT_DESC_SIZE
-	dd GDT_DESC_ADDRESS
+GDT32_BASE: dq 0x0
+GDT32_CODE: dq 0x00cf9a000000ffff
+GDT32_DATA: dq 0x00cf92000000ffff
+GDT32_DESC:
+	dw (($ - GDT32_BASE) - 1)
+	dd GDT32_BASE
 
 MESSAGE_UNSUPPORTED_CPUID: db "ERROR: This machine does not support the CPUID instruction", 0
-MESSAGE_UNSUPPORTED_64BIT: db "ERROR: This image is built for 64-bit mode, which is not supported by this machine", 0
-MESSAGE_TEST: db "REACHED HERE!!!", 14, 10, 0
-
+MESSAGE_DISK_ERROR: db "ERROR: Could not read from disk", 14, 10, 0
 
 times (510 - ($ - $$)) db 0
 dw 0xaa55  ; MBR signature
@@ -128,7 +98,5 @@ dw 0xaa55  ; MBR signature
 KERNEL_CODE equ $$ + 512
 VIDEO_MEMORY equ 0xb8000
 WHITE_ON_BLACK equ 0x1b
-GDT_DESC_SIZE equ ((gdt_end - gdt_begin) - 1)
-GDT_DESC_ADDRESS equ gdt_null
-CODE_SEG_OFFSET equ (gdt_code - gdt_begin)
-DATA_SEG_OFFSET equ (gdt_data - gdt_begin)
+CODE_SEG_OFFSET equ (GDT32_CODE - GDT32_BASE)
+DATA_SEG_OFFSET equ (GDT32_DATA - GDT32_BASE)
--- build.bat
+++ build.bat
@@ -6,12 +6,11 @@ set bin_filename="build\kernel.bin"
 set obj_filename="build\kernel.obj"
 
 call nasm bootstrap.asm -f bin -o %img_filename%
-call clang main.c -c --target=i386-pc-none-elf -ffreestanding -o %obj_filename%
+call clang main.c -c -m32 --target=x86_64-pc-none-elf -ffreestanding -o %obj_filename%
 call ld.lld %obj_filename% --oformat=binary --section-start=.text=0x7e00 -o %bin_filename%
 
 type %bin_filename% >> %img_filename%
 
 if "%1" equ "run" (
-	rem call qemu-system-x86_64w %img_filename%
-	call qemu-system-i386w %img_filename%
+	call qemu-system-x86_64w %img_filename%
 )
