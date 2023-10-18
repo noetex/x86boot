f279e9f35ffd4d9a7b5da4ece5797f95b33a543e
Rami Abbas <145647517+noetex@users.noreply.github.com>
2023-10-18 09:36:41 +0300

    clear VGA screen buffer

--- main.c
+++ main.c
@@ -1,27 +1,52 @@
 #include<stdint.h>
 
-#define WHITE_ON_BLACK 0x1b
+#define WHITE_ON_BLACK 0xf
+#define VGA_BUFFER_ADDRESS 0xb8000
+
+typedef int32_t size_t;
 
 typedef struct
 {
 	int8_t Character;
 	int8_t Attribute;
-} video_memory_cell;
+} vga_buffer_cell;
+
+static void*
+memset(void* Dest, int Pattern, size_t NumBytes)
+{
+	char* DestByte = Dest;
+	char ByteValue = (char)Pattern;
+	while(NumBytes > 0)
+	{
+		*DestByte = ByteValue;
+		DestByte += 1;
+		NumBytes -= 1;
+	}
+	void* Result = (void*)DestByte;
+	return Result;
+}
 
 static void
-print_string_to_video_memory(char* String, int8_t Attribute)
+vga_clear_screen(void)
 {
-	video_memory_cell* VideoMemory = (video_memory_cell*)0xb8000;
+	memset((void*)VGA_BUFFER_ADDRESS, 0, sizeof(vga_buffer_cell) * 80 * 25);
+}
+
+static void
+vga_print_string(char* String, int8_t Attribute)
+{
+	vga_buffer_cell* Cell = (vga_buffer_cell*)VGA_BUFFER_ADDRESS;
 	while(*String)
 	{
-		VideoMemory->Character = *String;
-		VideoMemory->Attribute = Attribute;
-		VideoMemory += 1;
+		Cell->Character = *String;
+		Cell->Attribute = Attribute;
+		Cell += 1;
 		String += 1;
 	}
 }
 
 void _main(void)
 {
-	print_string_to_video_memory("Welcome to the matrix!", WHITE_ON_BLACK);
+	vga_clear_screen();
+	vga_print_string("Welcome to the matrix!", WHITE_ON_BLACK);
 }
